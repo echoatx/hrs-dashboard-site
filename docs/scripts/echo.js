ff283c374708dee2b9671d6db76ee4c58bf46b78
Bryce Carson <bcars268@mtroyal.ca>
2025-04-29 22:09:15 -0600

    Add all files to update the dashboard; there are unnecessary files
    The dashboard needs to be updated, and I know what I'm pushing works with a local web server (used through node).
    
    The node_modules probably aren't required, but I need to get the site live, so everything is going in just so I can be sure I'm not missing anything.

--- docs/scripts/echo.js
+++ docs/scripts/echo.js
@@ -1,40 +1,26 @@
-function toggleFullscreen()
-{
-  var element = document.documentElement;
-  
-  var isInFullScreen = (document.fullScreenElement && document.fullScreenElement !== null) ||  (document.mozFullScreen || document.webkitIsFullScreen);
-  
-  if (isInFullScreen)
-  {
-    if (document.exitFullscreen)
-    {
-      document.exitFullscreen();
+function toggleFullscreen() {
+    var element = document.documentElement;
+
+    var isInFullScreen = (document.fullScreenElement && document.fullScreenElement !== null) || (document.mozFullScreen || document.webkitIsFullScreen);
+
+    if (isInFullScreen) {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    } else {
+        if (element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if (element.webkitRequestFullscreen) {
+            element.webkitRequestFullscreen();
+        } else if (element.msRequestFullscreen) {
+            element.msRequestFullscreen();
+        }
     }
-    else if (document.webkitExitFullscreen)
-    {
-      document.webkitExitFullscreen();
-    }
-    else if (document.msExitFullscreen)
-    {
-      document.msExitFullscreen();
-    }
-  }
-  else
-  {
-    if (element.requestFullscreen)
-    {
-      element.requestFullscreen();
-    }
-    else if (element.webkitRequestFullscreen)
-    {
-      element.webkitRequestFullscreen();
-    }
-    else if (element.msRequestFullscreen)
-    {
-      element.msRequestFullscreen();
-    }
-  }
-  return false;
+    return false;
 }
 
 var div = document.getElementById('fsButtonForToc');
